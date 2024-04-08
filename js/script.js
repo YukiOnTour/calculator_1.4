1a5c39aae38727ce5af5af6a10ad8c0434a00a52
Julian Yuki Morhard <111897050+YukiOnTour@users.noreply.github.com>
2024-04-08 10:19:57 +0200

    Added functions to js.script and created GitHub repository
    An add function: this should add two numbers (parameters) and return the sum.
    A subtract function: this should subtract the second number from the first number and return the difference.
    A multiply function: this should multiply two numbers and return the product.
    A divide function: this should divide the first number by the second number and return the quotient; however, if the divisor is equal to zero, the function should return the text “Not Allowed.”

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//Add function: adds two parameters and returns the sum
+function add(num1, num2){
+return num1 + num2;
+}
+
+//Subtract function: subtracts the second number from the first number and returns the difference
+function subtract(num1, num2){
+  return num1 - num2;
+  }
+
+//Multiply function: multiplies two numbers and returns the product
+function multiply(num1, num2){
+  return num1 * num2;
+  }
+
+// 
+function divide (num1, num2){
+  if (num2 === 0){
+    return "Not Allowed";
+  }
+  else {return num1 / num2;}
+  }
