5f4bba75a9994f3facd816e632bea354a35f4850
Julian Yuki Morhard <111897050+YukiOnTour@users.noreply.github.com>
2024-04-08 11:59:19 +0200

    Update script.js
    Added comments to divide function, which explain how the function works

--- js/script.js
+++ js/script.js
@@ -13,10 +13,12 @@ function multiply(num1, num2){
   return num1 * num2;
   }
 
-// 
+//Divide function: 
+//If the divisor is equal to zero, the function returns the string 'Not Allowed' 
 function divide (num1, num2){
   if (num2 === 0){
-    return "Not Allowed";
+    return 'Not Allowed';
   }
+  //Divides the first number by the second number and returns the quotient
   else {return num1 / num2;}
   }
